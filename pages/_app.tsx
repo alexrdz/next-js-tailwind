6a2e446cb617c65b00f298084e3c305ad14ba159
AlexRdz <drinkhorchata@gmail.com>
2020-08-24 19:31:16 -0500

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/styles.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        <h1>NextJS</h1>
+      </main>
+
+      
+    </div>
+  )
+}
